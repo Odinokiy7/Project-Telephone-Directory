2247ff159cded05d6fd728a611bb728ccd6bc684
Odinokiy7 <koazo-maga@mail.ru>
2022-05-27 10:49:01 +0300

    input function

---
+++ input_functions.py
@@ -0,0 +1,15 @@
+def get_surname():
+    surname = input('Surname: ')
+    return surname
+
+def get_name():
+    name = input('Name: ')
+    return name
+
+def get_number():
+    number = input('Namber: ')
+    return number
+
+def get_description():
+    description = input('Description: ')
+    return description
