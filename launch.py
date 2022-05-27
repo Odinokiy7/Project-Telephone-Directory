1d62cd155652c87493e23e7a115b9ee7ca867d81
Odinokiy7 <koazo-maga@mail.ru>
2022-05-27 10:49:23 +0300

    launch

---
+++ launch.py
@@ -0,0 +1,10 @@
+from enter_data import surname, name, number, description
+from type_recording import table_format, row_format
+
+surname()
+name()
+number()
+description()
+
+table_format()
+row_format()
